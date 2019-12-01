b8ec449ce9e6ccf943915863acc3612a255c842e
Davide Bontempelli <davide.bonte@gmail.com>
2019-12-01 21:16:28 +0100

    enable console log in prod

--- .eslintrc.js
+++ .eslintrc.js
@@ -48,8 +48,6 @@ module.exports = {
     'import/no-extraneous-dependencies': 'off',
     'prefer-promise-reject-errors': 'off',
 
-    // allow console.log during development only
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     // allow debugger during development only
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
